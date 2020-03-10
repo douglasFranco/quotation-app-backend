961459fde22101fe2c8b6ff3759be64f69f16664
Douglas Robert di Franco <douglas.franco@pampa.compasso>
2020-03-10 17:15:37 -0300

    fix db env var

--- src/config/database.js
+++ src/config/database.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
 
-const url = process.env.MONGOLAB_URI ? process.env.MONGOLAB_URI : 'mongodb://localhost/quotation'
+const url = process.env.MONGODB_URI ? process.env.MONGOLAB_URI : 'mongodb://localhost/quotation'
 module.exports = mongoose.connect(url, {useMongoClient: true})
