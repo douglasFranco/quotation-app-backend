1a15997b8b6470ea38f9a3f3cc253f44dc98c4ab
Douglas Robert di Franco <douglas.franco@pampa.compasso>
2020-03-10 21:53:03 -0300

    Fix file reference

--- src/config/database.js
+++ src/config/database.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
 
-const url = process.env.MONGODB_URI ? process.env.MONGOLAB_URI : 'mongodb://localhost/quotation'
+const url = 'mongodb://heroku_w2snbhh9:f0no1fdbmkjol38ufe4ji666k9@ds043467.mlab.com:43467/heroku_w2snbhh9'
 module.exports = mongoose.connect(url, {useMongoClient: true})
